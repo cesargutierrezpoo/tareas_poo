62019ca7cb8df104126ce9ff58c11fc17868b064
cesargutierrezpoo <>
2020-11-09 17:11:32 -0600

    quinto commit para tarea módulo 1, ejercicio 5

---
+++ src/ejercicioscesargutierrez/Ejercicio5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioscesargutierrez;
+
+/**
+ *
+ * @author vmbv2
+ */
+public class Ejercicio5 {
+    public static void main(String[] args){
+        
+        int i;
+        
+        for (i = 1; i < 51; i++) {
+            System.out.println(i*2);
+        
+        }
+    }
+    
+}
