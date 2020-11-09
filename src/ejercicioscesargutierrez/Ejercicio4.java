af7ad28966f63c34a34fcf85be1f0d76d0ff34a2
cesargutierrezpoo <>
2020-11-09 17:10:14 -0600

    cuarto commit para tarea módulo 1, ejercicio 4

---
+++ src/ejercicioscesargutierrez/Ejercicio4.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioscesargutierrez;
+
+/**
+ *
+ * @author vmbv2
+ */
+public class Ejercicio4 {
+    public static void main(String[] args){
+        
+        String[] ciudad = new String[20];
+        int i;
+        
+        ciudad[0] = "Tegucigalpa";
+        ciudad[1] = "San Salvador";
+        ciudad[2] = "Miami";
+        ciudad[3] = "París";
+        ciudad[4] = "Oregon";
+        ciudad[5] = "Choluteca";
+        ciudad[6] = "Managua";
+        ciudad[7] = "San José";
+        ciudad[8] = "Buenos Aires";
+        ciudad[9] = "Bogotá";
+        ciudad[10] = "Choloma";
+        ciudad[11] = "La Ceiba";
+        ciudad[12] = "Chiapas";
+        ciudad[13] = "Nueva York";
+        ciudad[14] = "Cartago";
+        ciudad[15] = "Barcelona";
+        ciudad[16] = "Roma";
+        ciudad[17] = "Helsinki";
+        ciudad[18] = "Dubai";
+        ciudad[19] = "Dortmund";
+        
+        System.out.println("20 Ciudades del mundo:");
+        System.out.println("----------------------------");
+        
+        for (i = 0; i < 20; i++) {
+            System.out.println(ciudad[i]);
+
+        }
+        
+        
+
+    
+        
+    }
+}
